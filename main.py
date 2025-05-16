2feb35ad1c0d605de2fd225a3e1f242c8cf0b151
yukinattu <phononeue@gmail.com>
2025-05-16 21:56:42 +0900

    first commit

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ try:
     initialize()
 except Exception as e:
     logger.error(f"{ct.INITIALIZE_ERROR_MESSAGE}\n{e}")
-    st.error(utils.build_error_message(ct.INITIALIZE_ERROR_MESSAGE), icon=ct.ERROR_ICON)
+    st.error(f"❌ 初期化エラーの詳細: {e}", icon="🚨")
     st.stop()
 
 # アプリ起動時のログ出力
